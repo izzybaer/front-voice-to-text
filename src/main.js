064954754087ad07afc5289c8f633005d7973061
Spencer Gietzen <Speng1516@comcast.net>
2017-08-20 17:14:21 -0700

    fixed webpack config to not be es6 import. changed a couple links so now webpack server will start and there arent any errors

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './style/main.scss'
+import './styles/main.scss'
 import React from 'react'
 import ReactDom from 'react-dom'
 import {Provider} from 'react-redux'
 
-import App from './components/app.js'
+import App from './components/app'
 import storeCreate from './lib/store-create.js'
 
 const store = storeCreate()
--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,11 @@
 require('dotenv').config()
 const production = process.env.NODE_ENV === 'production'
 
-// const {DefinePlugin, EnvironmentPlugin} = require('webpack')
-// const HTMLPlugin = require('html-webpack-plugin')
-// const CleanPlugin = require('clean-webpack-plugin')
-// const UglifyPlugin = require('uglifyjs-webpack-plugin')
-// const ExtractPlugin = require('extract-text-webpack-plugin')
-
-import {DefinePlugin, EnvironmentPlugin} from 'webpack'
-import HTMLPlugin from 'html-webpack-plugin'
-import CleanPlugin from 'clean-webpack-plugin'
-import UglifyPlugin from 'uglifyjs-webpack-plugin'
-import ExtractPlugin from 'extract-text-webpack-plugin'
+const {DefinePlugin, EnvironmentPlugin} = require('webpack')
+const HTMLPlugin = require('html-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 let plugins = [
   new EnvironmentPlugin(['NODE_ENV']),
@@ -28,8 +22,7 @@ let plugins = [
 if(production)
   plugins = plugins.concat([new CleanPlugin(), new UglifyPlugin()])
 
-// module.exports =
-export default {
+module.exports = {
   plugins,
   entry: `${__dirname}/src/main.js`,
   devServer: { historyApiFallback: true },
