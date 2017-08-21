221f27aa83cbdcb45907e506c9f0b569ea17dd93
Spencer Gietzen <Speng1516@comcast.net>
2017-08-21 15:10:02 -0700

    changed doc reducer

--- src/reducers/documents.js
+++ src/reducers/documents.js
@@ -1,14 +1,13 @@
-let initialState = []
+let initialState = {}
 export default (state = initialState, {type, payload}) => {
   switch(type) {
-  case 'document_CREATE':
-    return [...state, payload]
+  case 'DOCUMENT_CREATE':
+    return payload
   case 'DICTATION_UPDATE':
-    return state.map(document =>
-      document.id == payload.id ? payload : document)
+    return payload
   case 'DICTATION_DELETE':
-    return state.filter(document => document._id !== payload)
-  case 'document_FETCH':
+    return initialState
+  case 'DOCUMENT_FETCH':
     return payload
   case 'DOCUMENT_RESET':
     return initialState
