cba4f0b9399e8bc981e836982fa752cca3aea1bb
Spencer Gietzen <Speng1516@comcast.net>
2017-08-24 17:39:47 -0700

    added a create document form to the landing page, redirects to the view page with the correct url and doc values

--- src/components/document-landing-container/index.js
+++ src/components/document-landing-container/index.js
@@ -2,28 +2,71 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 import * as util from '../../lib/util.js'
+import * as document from '../../actions/document-actions.js'
 
 export class DocumentLandingContainer extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      title: '',
+      description: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleDocumentCreate = this.handleDocumentCreate.bind(this)
+  }
+
+  handleChange(event) {
+    let {name, value} = event.target
+    this.setState({[name]: value})
+  }
+
+  handleDocumentCreate(event) {
+    event.preventDefault()
+
+    this.props.documentCreate({
+      title: event.target.title.value,
+      description: event.target.description.value,
+    })
+      .then(() => {
+        this.props.history.push(`/document/${this.props.document._id}`)
+      })
   }
 
   render() {
     return (
       <div className='document-landing-container'>
-        <button onClick={this.handleDocumentCreate}>New Document</button>
+        <form className='create-doc' onSubmit={this.handleDocumentCreate}>
+          <h3>New Document</h3>
+          <input
+            name='title'
+            type='text'
+            value={this.state.title}
+            onChange={this.handleChange}
+            placeholder='Title'
+            required
+          />
+          <input
+            name='description'
+            type='text'
+            value={this.state.description}
+            onChange={this.handleChange}
+            placeholder='Description'
+            required
+          />
+          <button type='submit'>New Document</button>
+        </form>
       </div>
     )
   }
 }
 
 export const mapStateToProps = state => ({
-
+  document: state.document,
 })
 
 export const mapDispatchToProps = dispatch => ({
-
+  documentCreate: doc => dispatch(document.documentCreateRequest(doc)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(DocumentLandingContainer)
