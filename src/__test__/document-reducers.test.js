4f17d6f7598368bdc46ec5927a7f2643ec21ca2e
Spencer Gietzen <Speng1516@comcast.net>
2017-08-25 12:46:36 -0700

    added doc fetch all test and fixed the original doc fetch one test

--- src/__test__/document-reducers.test.js
+++ src/__test__/document-reducers.test.js
@@ -1,12 +1,14 @@
 import docReducer from '../reducers/documents.js'
 
 describe('Testing document reducer', () => {
+
   test('Default case', () => {
     let action = {type: 'INVALID'}
     let initialState = {doesnt: 'matter'}
     let result = docReducer(initialState, action)
-    expect(result).toBe(initialState)
+    expect(result).toEqual(initialState)
   })
+
   test('DOCUMENT_CREATE', () => {
     let action = {
       type: 'DOCUMENT_CREATE',
@@ -16,8 +18,9 @@ describe('Testing document reducer', () => {
       },
     }
     let result = docReducer(undefined, action)
-    expect(result).toBe(action.payload)
+    expect(result).toEqual(action.payload)
   })
+
   test('DOCUMENT_UPDATE', () => {
     let action = {
       type: 'DOCUMENT_UPDATE',
@@ -27,8 +30,9 @@ describe('Testing document reducer', () => {
       },
     }
     let result = docReducer(undefined, action)
-    expect(result).toBe(action.payload)
+    expect(result).toEqual(action.payload)
   })
+
   test('DOCUMENT_DELETE', () => {
     let action = {
       type: 'DOCUMENT_DELETE',
@@ -37,24 +41,42 @@ describe('Testing document reducer', () => {
       },
     }
     let result = docReducer(undefined, action)
-    expect(result).toEqual({})
+    expect(result).toEqual([])
   })
-  test('DOCUMENT_FETCH', () => {
+
+  test('DOCUMENT_FETCH_ONE', () => {
     let action = {
-      type: 'DOCUMENT_FETCH',
+      type: 'DOCUMENT_FETCH_ONE',
       payload: {
         id: 123,
         text: 'fetch',
       },
     }
     let result = docReducer(undefined, action)
-    expect(result).toBe(action.payload)
+    expect(result).toEqual([action.payload])
   })
+
+  test('DOCUMENT_FETCH_ALL', () => {
+    let action = {
+      type: 'DOCUMENT_FETCH_ALL',
+      payload: [{
+        id: 123,
+        text: 'fetch',
+      },
+      {
+        id: 3333,
+        text: 'fettttch',
+      }],
+    }
+    let result = docReducer(undefined, action)
+    expect(result).toEqual([...action.payload])
+  })
+
   test('DOCUMENT_RESET', () => {
     let action = {
       type: 'DOCUMENT_RESET',
     }
     let result = docReducer(undefined, action)
-    expect(result).toEqual({})
+    expect(result).toEqual([])
   })
 })
