0b6ff93a1a3d9e1ecc44cece86a5efa592bb59ec
Spencer Gietzen <Speng1516@comcast.net>
2017-08-25 12:51:19 -0700

    added coverage to test and added test-watch to packagejson. fixed fetch to fetchone and added fetchall to action tests

--- src/__test__/document-actions.test.js
+++ src/__test__/document-actions.test.js
@@ -1,39 +1,53 @@
 import * as document from '../actions/document-actions.js'
 
 describe('Testing document actions', () => {
+
   test('documentCreate', () => {
     let dummyDoc = {
       doesnt: 'matter',
     }
     let result = document.documentCreate(dummyDoc)
-    expect(result.type).toBe('DOCUMENT_CREATE')
-    expect(result.payload).toBe(dummyDoc)
+    expect(result.type).toEqual('DOCUMENT_CREATE')
+    expect(result.payload).toEqual(dummyDoc)
   })
+
   test('documentUpdate', () => {
     let dummyDoc = {
       doesnt: 'matter',
     }
     let result = document.documentUpdate(dummyDoc)
-    expect(result.type).toBe('DOCUMENT_UPDATE')
-    expect(result.payload).toBe(dummyDoc)
+    expect(result.type).toEqual('DOCUMENT_UPDATE')
+    expect(result.payload).toEqual(dummyDoc)
   })
+
   test('documentDelete', () => {
     let id = 123
     let result = document.documentDelete(id)
-    expect(result.type).toBe('DOCUMENT_DELETE')
+    expect(result.type).toEqual('DOCUMENT_DELETE')
     expect(result.payload).toBeUndefined()
   })
-  test('documentFetch', () => {
+
+  test('documentFetchOne', () => {
     let dummyDoc = {
       doesnt: 'matter',
     }
-    let result = document.documentFetch(dummyDoc)
-    expect(result.type).toBe('DOCUMENT_FETCH')
-    expect(result.payload).toBe(dummyDoc)
+    let result = document.documentFetchOne(dummyDoc)
+    expect(result.type).toEqual('DOCUMENT_FETCH_ONE')
+    expect(result.payload).toEqual(dummyDoc)
   })
+
+  test('documentFetchAll', () => {
+    let dummyDoc = {
+      doesnt: 'matter',
+    }
+    let result = document.documentFetchAll(dummyDoc)
+    expect(result.type).toEqual('DOCUMENT_FETCH_ALL')
+    expect(result.payload).toEqual(dummyDoc)
+  })
+
   test('documentReset', () => {
     let result = document.documentReset()
-    expect(result.type).toBe('DOCUMENT_RESET')
+    expect(result.type).toEqual('DOCUMENT_RESET')
     expect(result.payload).toBeUndefined()
   })
 })
