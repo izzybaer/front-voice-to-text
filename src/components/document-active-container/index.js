11509a844c0901e40760a1588ff172a5e3ba824f
Spencer Gietzen <Speng1516@comcast.net>
2017-08-25 19:04:54 -0700

    added the save button to save changes made to a document to the db, no socket io yet

--- src/components/document-active-container/index.js
+++ src/components/document-active-container/index.js
@@ -7,19 +7,40 @@ import * as document from '../../actions/document-actions.js'
 export class DocumentActiveContainer extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      title: '',
+      description: '',
+      body: '',
+    }
 
     this.handleChange = this.handleChange.bind(this)
+    this.handleSave = this.handleSave.bind(this)
+  }
+
+  handleSave(event) {
+    event.preventDefault()
+    let targ = event.target
+    let updatedDoc = {
+      _id: this.props.match.params[0],
+      title: targ.title.value,
+      description: targ.description.value,
+      body: targ.body.value,
+    }
+    this.props.documentUpdate(updatedDoc)
+      .then(doc => {
+        this.props.documentFetchOne(doc.body._id)
+      })
   }
 
   handleChange(event) {
     let {name, value} = event.target
-    let updatedDoc = {
-      ...this.props.document,
-      [name]: value,
-    }
-    util.log('updatedDoc:', updatedDoc)
-    this.props.documentUpdate(updatedDoc)
+    this.setState({ [name]: value })
+    // let updatedDoc = {
+    //   ...this.props.document,
+    //   [name]: value,
+    // }
+    // util.log('updatedDoc:', updatedDoc)
+    // this.props.documentUpdate(updatedDoc)
   }
 
   componentWillMount() {
@@ -28,32 +49,35 @@ export class DocumentActiveContainer extends React.Component {
       this.props.history.push('/')
     util.log('url param:', id)
     this.props.documentFetchOne(id)
+      .then(doc => {
+        this.setState({...doc.body})
+      })
   }
 
   render() {
-    util.log('props', this.props.document)
+    util.log('props', this.state)
     return (
       <div className='document-active-container'>
         {this.props.document
-          ? <form onSubmit={this.handleSubmit} name='active-doc'>
+          ? <form onSubmit={this.handleSave} name='active-doc'>
             <input
               name='title'
               type='text'
               placeholder='Title'
-              value={this.props.document.title}
+              value={this.state.title}
               onChange={this.handleChange}
             />
             <input
               name='description'
               type='text'
               placeholder='Description'
-              value={this.props.document.description}
+              value={this.state.description}
               onChange={this.handleChange}
             />
             <textarea
               name='body'
               placeholder='Document body'
-              value={this.props.document.body}
+              value={this.state.body}
               onChange={this.handleChange}
             />
             <button type='submit'>Save</button>
@@ -71,7 +95,7 @@ export const mapStateToProps = state => ({
 
 export const mapDispatchToProps = dispatch => ({
   documentFetchOne: id => dispatch(document.documentFetchOneRequest(id)),
-  documentUpdate: document => dispatch(document.documentUpdateRequest(document)),
+  documentUpdate: newDoc => dispatch(document.documentUpdateRequest(newDoc)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(DocumentActiveContainer)
