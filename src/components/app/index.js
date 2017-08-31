786113ad2725a97e344424c6d7a2e69bbb8950f9
Spencer Gietzen <Speng1516@comcast.net>
2017-08-31 15:58:21 -0700

    added logic to re-login if a user visits the site with an existing cookie

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {BrowserRouter, Link, Route} from 'react-router-dom'
 
 import * as util from '../../lib/util.js'
+import * as auth from '../../actions/auth-actions.js'
 
 import HeaderContainer from '../header-container'
 import AuthContainer from '../auth-container'
@@ -13,6 +14,12 @@ export class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
+  }
+
+  componentDidMount() {
+    let token = util.cookieFetch('X-VtT-Token')
+    if(token)
+      this.props.tokenSet(token)
   }
 
   render() {
@@ -32,11 +39,11 @@ export class App extends React.Component {
 }
 
 export const mapStateToProps = state => ({
-
+  token: state.token,
 })
 
 export const mapDispatchToProps = dispatch => ({
-
+  tokenSet: token => dispatch(auth.tokenSet(token)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
