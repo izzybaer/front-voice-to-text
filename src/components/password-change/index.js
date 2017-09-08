41d0aaedede4bbd1c32f72d0bf1958aaeea20296
Spencer Gietzen <Speng1516@comcast.net>
2017-09-07 18:16:57 -0700

    password change component

---
+++ src/components/password-change/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export class PasswordChange extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <form className='password-change'>
+
+      </form>
+    )
+  }
+}
