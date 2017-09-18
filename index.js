d881328676b4fab043c208a026559057c4b49f46
Spencer Gietzen <Speng1516@comcast.net>
2017-09-18 15:04:22 -0700

    added cors and dotenv

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+require('dotenv').config()
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
 
 app.use(morgan('common'))
+app.use(cors({
+  origin: process.env.CORS_ORIGINS,
+  credentials: true,
+}))
 app.use(express.static(`${__dirname}/build`))
 app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
 
